01b8d06f6a47f194f6012c311d7dd3c4d5c9739f
jaeseongDev <qkrwotjd1445@naver.com>
2026-01-02 15:29:49 +0900

    feat: 좋아요 API 추가

---
+++ src/main/java/com/example/redisintermediateproject/like/LikeService.java
@@ -0,0 +1,18 @@
+package com.example.redisintermediateproject.like;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+    private final LikeRepository likeRepository;
+
+    public void likePost(LikePostRequestDto likePostRequestDto) {
+        Long userId = likePostRequestDto.getUserId();
+        Long postId = likePostRequestDto.getPostId();
+        Like like = new Like(userId, postId);
+        likeRepository.save(like);
+    }
+}
